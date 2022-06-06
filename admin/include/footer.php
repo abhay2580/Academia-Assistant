b00175c5791e12d2698ff37abb90599df60e6059
Abhay <abhayyadav.s2mca21@bvicam.in>
2022-06-06 20:07:35 +0530

    Initial Commit

---
+++ admin/include/footer.php
@@ -0,0 +1,12 @@
+<footer>
+      <div class="bottom-details">
+      <div class="bottom_text">
+        <span class="copyright_text">Copyright © 2022 <a href="index.html">Academia Assistant.</a>All rights reserved</span>
+        <span class="policy_terms">
+          <span class= "ftbld" >Created by : </span>
+          <a href="https://www.linkedin.com/in/abhay-yadav-23a108227/" target="_blank"> Abhay Yadav</a>
+          <a href="https://www.linkedin.com/in/akmallick/" target="_blank">Ashish Kumar Mallick</a> 
+        </span>
+      </div>
+    </div>
+  </footer>
